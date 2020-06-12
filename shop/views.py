475062884f29df6796b7a684d425e0bce48a00e3
Aditya Taksande <taksandeaditya@gmail.com>
2020-06-12 11:09:37 +0530

    added items in db, changed views, updated logic

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render,HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request,'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4)) 
+    params = {'no_of_slides':nSlides,'range':range(1,nSlides),'product': products}
+
+    return render(request,'shop/index.html',params)
 
 
 def about(request):
