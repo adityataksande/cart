975b9d5862b46167cf9884725613d460ae3bd625
Aditya Taksande <taksandeaditya@gmail.com>
2020-06-13 16:12:15 +0530

    updated views, style, index

--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,14 @@ from .models import Product
 from math import ceil
 
 # Create your views here.
+
+
 def index(request):
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
     allProds = []
     catprods = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catprods}
@@ -15,10 +22,9 @@ def index(request):
 
     # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
     # allProds = [[products, range(1, nSlides), nSlides],
-    #             [products, range(1, nSlides), nSlides]] 
+    #             [products, range(1, nSlides), nSlides]]
     params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
-
 
 
 def about(request):
